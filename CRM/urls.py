320543b1575402e9557daf173eb9e5ea12ea10cb
18829760640 <realbyh@qq.com>
2024-11-26 23:23:06 +0800

    inti

--- CRM/urls.py
+++ CRM/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('index', views.index),
+    path('main', views.main),
 ]
--- web01/models.py
+++ web01/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=32)
+    age = models.IntegerField()
+    email = models.EmailField()
+    company = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32)
+    create_time = models.DateTimeField(auto_now_add=True)
+
